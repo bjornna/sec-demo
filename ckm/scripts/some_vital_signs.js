ae43c71f91b1b8d8a8c51e0c20738c1488f9a203
Bjørn Næss <bna@dips.no>
2018-11-03 15:49:12 +0100

    Formatting

--- ckm/scripts/some_vital_signs.js
+++ ckm/scripts/some_vital_signs.js
@@ -11,42 +11,41 @@ var SYSTOLIC = "encounter/blood_pressure/any_event/systolic";
 var DIASTOLIC = "encounter/blood_pressure/any_event/diastolic";
 var MEAN_ARTERIAL_PRESSURE =
   "encounter/blood_pressure/any_event/mean_arterial_pressure";
-  var BP_TIME = "encounter/blood_pressure/any_event/time";
+var BP_TIME = "encounter/blood_pressure/any_event/time";
 
-  var isChangingMethod = false;
+var isChangingMethod = false;
 
-  console.log("I am here");
-  function canChange(){
-      return isChangingMethod;
+console.debug("Script is loaded");
+function canChange() {
+  return isChangingMethod;
+}
+
+api.addListener(BMI, "onChildAdded", function(id, value, parent) {
+  console.log("BMI added");
+  updateBMI();
+});
+api.addListener(BMI, "OnFormInitialized", function(id, value, parent) {
+  console.log("OnFormInitialized");
+  updateBMI();
+});
+
+api.addListener(WEIGHT, "OnChanged", function(id, value, parent) {
+  updateBMI();
+});
+api.addListener(HEIGHT, "OnChanged", function(id, value, parent) {
+  updateBMI();
+});
+api.addListener(BMI, "OnChanged", function(id, value, parent) {
+  if (canChange()) {
+    console.log("Do not change");
+  } else {
+    console.log("Can change");
+    var bmiRMMethod = api.getFieldValue(BMI_METHOD);
+    console.log(bmiRMMethod);
+    bmiRMMethod.DefiningCode.CodeString = "at0008";
+    api.setFieldValue(BMI_METHOD, bmiRMMethod);
   }
-
-  api.addListener(BMI, "onChildAdded", function(id,value, parent){
-console.log("BMI added");
-updateBMI();
-
-  });
-  api.addListener(BMI, "OnFormInitialized", function(id, value, parent){
-      console.log("OnFormInitialized");
-      updateBMI();
-  });
-
-  api.addListener(WEIGHT, "OnChanged", function(id, value, parent){
-      updateBMI();
-  });
-  api.addListener(HEIGHT, "OnChanged", function(id, value, parent){
-    updateBMI();
-  });
-  api.addListener(BMI, "OnChanged", function(id, value, parent){
-      if(canChange()){
-          console.log("Do not change");
-      }else{
-          console.log("Can change");
-          var bmiRMMethod = api.getFieldValue(BMI_METHOD);
-          console.log(bmiRMMethod);
-          bmiRMMethod.DefiningCode.CodeString = "at0008";
-          api.setFieldValue(BMI_METHOD, bmiRMMethod);
-      }
-  });
+});
 
 api.addListener(SYSTOLIC, "OnChanged", function(id, value, parent) {
   updateMAP(parent);
@@ -55,36 +54,33 @@ api.addListener(DIASTOLIC, "OnChanged", function(id, value, parent) {
   updateMAP(parent);
 });
 
-function updateBMI(){
-    var w = api.getFieldValue(WEIGHT).magnitude;
-    var h = api.getFieldValue(HEIGHT).magnitude;
-    
-    console.log("w=" + w + ", h=" + h);
-    if(h > 0 && w > 0){
-        isChangingMethod = true;
-        var bmi = calculateBMI(w,h);
-        console.log("BMI:" + bmi);
+function updateBMI() {
+  var w = api.getFieldValue(WEIGHT).magnitude;
+  var h = api.getFieldValue(HEIGHT).magnitude;
 
-        var bmiRM = new DvQuantity();
-        bmiRM.magnitude = bmi;
-        bmiRM.units = "kg/m2";
-        api.setFieldValue(BMI, bmiRM);
+  console.log("w=" + w + ", h=" + h);
+  if (h > 0 && w > 0) {
+    isChangingMethod = true;
+    var bmi = calculateBMI(w, h);
+    console.log("BMI:" + bmi);
 
+    var bmiRM = new DvQuantity();
+    bmiRM.magnitude = bmi;
+    bmiRM.units = "kg/m2";
+    api.setFieldValue(BMI, bmiRM);
 
-        var bmiRMMethod = api.getFieldValue(BMI_METHOD);
-        console.log(bmiRMMethod);
-        bmiRMMethod.DefiningCode.CodeString = "at0007";
+    var bmiRMMethod = api.getFieldValue(BMI_METHOD);
+    console.log(bmiRMMethod);
+    bmiRMMethod.DefiningCode.CodeString = "at0007";
 
-        var bmiTimeRm = new DvDateTime(new Date());
-        api.setFieldValue(BMI_TIME, bmiTimeRm);
+    var bmiTimeRm = new DvDateTime(new Date());
+    api.setFieldValue(BMI_TIME, bmiTimeRm);
 
-        
-        api.setFieldValue(BMI_METHOD, bmiRMMethod);
-        isChangingMethod = false;
-    }else{
-        console.log("Either w or h is not set");
-    }
-    
+    api.setFieldValue(BMI_METHOD, bmiRMMethod);
+    isChangingMethod = false;
+  } else {
+    console.log("Either w or h is not set");
+  }
 }
 
 function updateMAP(container) {
@@ -102,13 +98,9 @@ function updateMAP(container) {
   console.log(mapRM);
 }
 
-
-function calculateBMI(w, h){
-    var hCalc = h /100;
-    return round(
-        w / (hCalc * hCalc), 
-        2
-    );
+function calculateBMI(w, h) {
+  var hCalc = h / 100;
+  return round(w / (hCalc * hCalc), 2);
 }
 
 function caculateMeanArterialPressure(systolic, diastolic) {
